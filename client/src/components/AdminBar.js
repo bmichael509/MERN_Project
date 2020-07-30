c5aaf53d6ed89456271964909e9abc8804c939f4
bmichael509 <62362146+bmichael509@users.noreply.github.com>
2020-07-30 16:27:37 -0700

    Updated the admin bar with some additional styling

--- client/src/components/AdminBar.js
+++ client/src/components/AdminBar.js
@@ -1,19 +1,65 @@
 import React from 'react';
-import { Link } from '@reach/router';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { Link } from '@reach/router'
 
-const AdminBar = (props) => {
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    // necessary for content to be below app bar
+    toolbar: theme.mixins.toolbar,
+    content: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.default,
+        padding: theme.spacing(3),
+    },
+}));
+
+export default function PermanentDrawerLeft() {
+    const adminPages = ['Contracts', 'Customers', 'Employees', 'Issues', 'Properties', 'Units', 'Unit Types', 'Users'];
+    const adminLinks = ['contracts', 'customers', 'cmployees', 'issues', 'properties', 'units', 'unitTypes', 'users'];
+    const classes = useStyles();
+
     return (
-        <div className='adminHead'>
-            <nav className="adminNav">
-                <h3>Admin Pages</h3>
-                <Link to="/admin/customers">Customers</Link>
-                <Link to="/admin/units">Units</Link>
-                <Link to="/admin/unitTypes">Unit Types</Link>
-                <Link to="/admin/issues">Issues</Link>
-                <Link to="/admin/properties">Property</Link>
-            </nav>
+        <div className={classes.root}>
+            <CssBaseline />
+            <Drawer
+                className={classes.drawer}
+                variant="permanent"
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+                anchor="left"
+            >
+                <div className={classes.toolbar} />
+                <Divider />
+                <List>
+                    {adminPages.map((text, index) => (
+                        <ListItem button key={text} button component={Link} to={`/admin/${adminLinks[index]}`}>
+                            <ListItemText primary={text} />
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
         </div>
-    )
+    );
 }
-
-export default AdminBar;
