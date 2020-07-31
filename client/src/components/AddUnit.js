a8b524ff763e8f48aec2131c7edef964be80cc93
jpgarrett <jpgarrett24@gmail.com>
2020-07-31 13:25:33 -0700

    functional forms. Updated Material UI for property form and add unit form

--- client/src/components/AddUnit.js
+++ client/src/components/AddUnit.js
@@ -2,6 +2,15 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { navigate } from "@reach/router";
 import Loading from "./Loading";
+import {
+    Grid,
+    FormControl,
+    Container,
+    Select,
+    Button,
+    TextField
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 const AddUnit = (props) => {
     const { action, id } = props;
@@ -14,6 +23,29 @@ const AddUnit = (props) => {
     });
     const [unitTypes, setUnitTypes] = useState(null);
     const [properties, setProperties] = useState(null);
+
+    const useStyles = makeStyles((theme) => ({
+        TextField: {
+            padding: theme.spacing(5),
+            textAlign: 'center',
+            color: theme.palette.text.secondary,
+            whiteSpace: 'nowrap',
+            marginBottom: theme.spacing(5),
+        },
+
+    }));
+
+    function ContainedButtons() {
+        const classes = useStyles();
+    }
+
+    const styles = {
+        button: {
+            width: "100%",
+            backgroundColor: "blue",
+            margin: "10px"
+        }
+    }
 
     useEffect(() => {
         axios.get('http://localhost:8000/api/unitTypes')
@@ -93,40 +125,56 @@ const AddUnit = (props) => {
 
     return (
         <>
-            <form onSubmit={handleSubmit} className='addCustomer'>
-                <label htmlFor="name">Unit Name/Number:</label>
-                <input type="text" name="name" id="name" value={inputs.name} onChange={(event) => setInputs({ ...inputs, name: event.target.value })} />
-                <label htmlFor="status">Unit Status:</label>
-                <select name="status" id="status" value={inputs.status} onChange={(event) => setInputs({ ...inputs, status: event.target.value })}>
-                    <option value="">Select a unit status:</option>
-                    <option value="available">Available</option>
-                    <option value="rented">Rented</option>
-                    <option value="inactive">Inactive</option>
-                </select>
-                <label htmlFor="unitType">Unit Type:</label>
-                <select name="unitType" id="unitType" value={inputs.unitTypeID} onChange={(event) => setInputs({ ...inputs, unitTypeID: event.target.value })}>
-                    <option value="">Select a Unit Type</option>
-                    {unitTypes.map((unitType) => {
-                        return (
-                            <option key={unitType._id} value={unitType._id}>{unitType.name}</option>
-                        );
-                    })}
-                </select>
-                <label htmlFor="property">Property:</label>
-                <select name="property" id="property" value={inputs.propertyID} onChange={(event) => setInputs({ ...inputs, propertyID: event.target.value })}>
-                    <option value="" disabled={true}>Select Property</option>
-                    {properties.map((property) => {
-                        return (
-                            <option key={property._id} value={property._id}>{property.propertyName}</option>
-                        );
-                    })}
-                </select>
-                <label htmlFor="notes">Notes:</label>
-                <input type="text" name="notes" id="notes" value={inputs.notes} onChange={(event) => setInputs({ ...inputs, notes: event.target.value })} />
-                <button type="button" onClick={(event) => navigate('/admin/units')}>Cancel</button>
-                <button>Submit</button>
-            </form>
+            <Container fixed>
+                <FormControl onSubmit={handleSubmit} className='addUnit'>
+                    <h4>Add a New Unit:</h4>
+                    <Grid container spacing={2} justify="space-around" direction="row" alignItems="centered">
+                        <Grid container item justify="space-around">
+                            <div className='addPropertyInput'>
+                                <TextField variant="outlined" label="Unit Name/Number:" InputLabelProps={{ shrink: true, }} type="text" name="name" id="name" value={inputs.name} onChange={(event) => setInputs({ ...inputs, name: event.target.value })} />
+                            </div>
+                            <div className='addPropertyInput'>
+                                <Select native variant="outlined" name="status" id="status" value={inputs.status} onChange={(event) => setInputs({ ...inputs, status: event.target.value })}>
+                                    <option value="">Select a unit status:</option>
+                                    <option value="available">Available</option>
+                                    <option value="rented">Rented</option>
+                                    <option value="inactive">Inactive</option>
+                                </Select>
+                            </div>
+                            <div className='addPropertyInput'>
+                                <Select native variant="outlined" name="unitType" id="unitType" value={inputs.unitTypeID} onChange={(event) => setInputs({ ...inputs, unitTypeID: event.target.value })}>
+                                    <option value="">Select a Unit Type</option>
+                                    {unitTypes.map((unitType) => {
+                                        return (
+                                            <option key={unitType._id} value={unitType._id}>{unitType.name}</option>
+                                        );
+                                    })}
+                                </Select >
+                            </div>
+                            <div className='addPropertyInput'>
+                                <Select native variant="outlined" name="property" id="property" value={inputs.propertyID} onChange={(event) => setInputs({ ...inputs, propertyID: event.target.value })}>
+                                    <option value="" disabled={true}>Select Property</option>
+                                    {properties.map((property) => {
+                                        return (
+                                            <option key={property._id} value={property._id}>{property.propertyName}</option>
+                                        );
+                                    })}
+                                </Select>
+                            </div>
+                            <div className='addPropertyInput'>
+                                <TextField variant="outlined" label="Notes:" InputLabelProps={{ shrink: true, }} type="text" name="notes" id="notes" value={inputs.notes} onChange={(event) => setInputs({ ...inputs, notes: event.target.value })} />
+                            </div>
+                        </Grid>
+                        <Grid>
+
+                            <Button variant="contained" color="secondary" onClick={(event) => navigate('/admin/units')}>Cancel</Button>
+                            <Button variant="contained" color="primary" onClick={handleSubmit}>Submit</Button>
+                        </Grid>
+                    </Grid>
+                </FormControl>
+
+            </Container>
         </>
-    );
+    )
 };
 export default AddUnit;
